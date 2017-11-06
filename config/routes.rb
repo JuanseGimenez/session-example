20629bdd305321030a24ecf2a056d80e5dcec274
Juanse Gimenez <juansegimenez94@gmail.com>
2017-11-06 19:19:19 -0300

    Chat room scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'static_pages#index'
-
+  root 'chat_rooms#index'
+  resources :chat_rooms
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
