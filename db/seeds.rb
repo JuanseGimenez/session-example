b225249df1c231fed1570fd50904aa32d35ccf8b
Juanse Gimenez <juansegimenez94@gmail.com>
2017-11-07 09:50:23 -0300

    Added 2 chat rooms for example

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,9 @@
 
 user = User.create(email: 'example@example.com', password: 'password', password_confirmation: 'password')
 puts "User created for example email: #{user.email} password: #{user.password}"
+
+chat_room1 = ChatRoom.create(title: "Chat room with password", password: "password", user: user)
+chat_room2 = ChatRoom.create(title: "Chat room without password", user: user)
+
+puts "Chat room created #{chat_room1.title}"
+puts "Chat room created #{chat_room2.title}"
